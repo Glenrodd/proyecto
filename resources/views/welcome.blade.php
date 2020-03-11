f0d1e1ff6d3d2e8285abffd1155de73f9fc877bf
Gleney Roddwy GRLP. Limachi Poma <limachi.gleney@viasbolivia.gob.bo>
2020-03-11 16:19:09 -0400

    prueba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Spatie
                 </div>
 
                 <div class="links">
